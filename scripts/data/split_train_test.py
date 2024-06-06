33d402caf25964bdc37c13d8c1ca700afcb3e421
Igor Sulim <igosulim@gmail.com>
2024-06-06 15:45:06 +0200

    func to split and save

--- scripts/data/split_train_test.py
+++ scripts/data/split_train_test.py
@@ -30,10 +30,28 @@ def save_to_csv(x: pd.DataFrame, y: pd.DataFrame, out: Path) -> None:
     y.to_csv(Path(out, "y.csv"), index=False)
 
 
+def split_and_save(base_data_path: Path, split_size: float = 0.1, seed: int = 42) -> None:
+    """Split datasets and save to CSV files."""
+
+    x = pd.read_csv(Path(base_data_path, "preprocessed", "X_pre.csv"))
+    y = pd.read_csv(Path(base_data_path, "preprocessed", "y_pre.csv"))
+    x_train, y_train, x_val, y_val, x_test, y_test = split_train_val_test(x, y, size=0.1, seed=42)
+
+    train_path = Path(base_data_path, "train")
+    val_path = Path(base_data_path, "val")
+    test_path = Path(base_data_path, "test")
+
+    os.makedirs(train_path, exist_ok=True)
+    os.makedirs(val_path, exist_ok=True)
+    os.makedirs(test_path, exist_ok=True)
+
+    save_to_csv(x_train, y_train, train_path)
+    save_to_csv(x_val, y_val, val_path)
+    save_to_csv(x_test, y_test, test_path)
+
+
 if __name__ == "__main__":
     load_dotenv()
     root_data = os.getenv("KAGGLE_FILES_DIR")
     dataset_path = Path(os.getcwd(), root_data)
-    X = pd.read_csv(Path(dataset_path, "preprocessed", "X_pre.csv"))
-    y = pd.read_csv(Path(dataset_path, "preprocessed", "y_pre.csv"))
-    split_train_val_test(X, y, size=0.1, seed=42)
+    split_and_save(dataset_path)
