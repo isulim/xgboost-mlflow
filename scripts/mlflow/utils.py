2ef854600aadad6711d2a2ea0f8d3bb170a94667
Igor Sulim <igosulim@gmail.com>
2024-06-15 12:09:10 +0200

    dataclass for parameters validation

--- scripts/mlflow/utils.py
+++ scripts/mlflow/utils.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from dataclasses import dataclass, field
 from pathlib import Path
 
 from scripts.data.configure_envs import configure_envs
@@ -31,3 +32,31 @@ def get_metric(y_val, y_pred, metric_name: str):
 
     else:
         return metric_func(y_val, y_pred)
+
+
+@dataclass
+class ClassifierParams:
+    """Dataclass for classifier parameters."""
+
+    objective: str = field(default="binary:logistic")
+    eta: float = field(default=0.3, metadata={"alias": "learning_rate"})
+    gamma: float = field(default=0, metadata={"alias": "min_split_loss"})
+    max_depth: int = field(default=6)
+    min_child_weight: int = field(default=1)
+    max_delta_step: float = field(default=0.0)
+    subsample: float = field(default=0.8)
+    colsample_bytree: float = field(default=0.8)
+    lambda_: float = field(default=1.0, metadata={"alias": "reg_lambda"})
+    alpha: float = field(default=0.0, metadata={"alias": "reg_alpha"})
+    n_estimators: int = field(default=100)
+    max_leaf_nodes: int = field(default=None)
+    scale_pos_weight: float = field(default=1.0)
+    seed: int = field(default=42)
+
+
+def get_params(params: dict) -> ClassifierParams:
+    """Get parameters for model training."""
+
+    parameters = ClassifierParams(**params)
+
+    return parameters
