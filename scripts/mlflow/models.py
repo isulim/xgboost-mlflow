97f19b6a3a16ea841399d16d7bcff4e3be59344d
Igor Sulim <igosulim@gmail.com>
2024-06-15 15:22:06 +0200

    change value_type to be not optional

--- scripts/mlflow/models.py
+++ scripts/mlflow/models.py
@@ -32,11 +32,11 @@ class TrialParameter(BaseModel):
     """Base model of single optuna trial parameter."""
 
     name: str
-    value_type: str = Field("float", pattern="^(int|float|str|bool)$")
+    value_type: str = Field(pattern="^(int|float|str|bool)$")
     values: Optional[list[str | int | float | bool]] = None
     start: Optional[float | int] = Field(None, ge=0)
     end: Optional[float | int] = Field(None, ge=0)
-    step: Optional[float | None] = Field(None, ge=0)
+    step: Optional[float | int | None] = Field(None, ge=0)
     log: Optional[bool] = False
 
     class Config:
@@ -60,23 +60,32 @@ class TrialParameter(BaseModel):
         if params.start and params.start and params.start >= params.end:
             raise ValidationError("Field `start` must be less than `end`.")
 
+        if params.log is True and params.step != 1:
+            raise ValidationError("Field `step` must equal 1 for log scale.")
+
 
 class TrialParameters(BaseModel):
     """Model of optuna trial parameters."""
 
-    objective: TrialParameter = Field(TrialParameter(name="objective", values=["binary:logistic"]))
-    eta: TrialParameter = Field(TrialParameter(name="eta", start=0.2, end=0.3, step=0.01))
-    gamma: TrialParameter = Field(TrialParameter(name="gamma", start=0, end=0.1, step=0.01))
-    max_depth: TrialParameter = Field(TrialParameter(name="max_depth", start=4, end=6, step=1))
-    min_child_weight: TrialParameter = Field(TrialParameter(name="min_child_weight", start=1, end=3, step=1))
-    max_delta_step: TrialParameter = Field(TrialParameter(name="max_delta_step", start=0, end=0.1, step=0.01))
-    subsample: TrialParameter = Field(TrialParameter(name="subsample", start=0.7, end=0.9, step=0.1))
-    colsample_bytree: TrialParameter = Field(TrialParameter(name="colsample_bytree", start=0.7, end=0.9, step=0.1))
-    lambda_: TrialParameter = Field(TrialParameter(name="reg_lambda", start=1, end=3, step=1))
-    alpha: TrialParameter = Field(TrialParameter(name="reg_alpha", start=0, end=1, step=0.1))
-    n_estimators: TrialParameter = Field(TrialParameter(name="n_estimators", start=50, end=150, step=50))
-    max_leaf_nodes: TrialParameter = Field(TrialParameter(name="max_leaf_nodes", start=3, end=7, log=True))
-    scale_pos_weight: TrialParameter = Field(TrialParameter(name="scale_pos_weight", values=[1.0]))
+    objective: TrialParameter = Field(TrialParameter(name="objective", value_type="str", values=["binary:logistic"]))
+    eta: TrialParameter = Field(TrialParameter(name="eta", value_type="float", start=0.2, end=0.3, step=0.01))
+    gamma: TrialParameter = Field(TrialParameter(name="gamma", value_type="float", start=0, end=0.1, step=0.01))
+    max_depth: TrialParameter = Field(TrialParameter(name="max_depth", value_type="int", start=4, end=6, step=1))
+    min_child_weight: TrialParameter = Field(
+        TrialParameter(name="min_child_weight", value_type="int", start=1, end=3, step=1))
+    max_delta_step: TrialParameter = Field(
+        TrialParameter(name="max_delta_step", value_type="float", start=0, end=0.1, step=0.01))
+    subsample: TrialParameter = Field(
+        TrialParameter(name="subsample", value_type="float", start=0.7, end=0.9, step=0.1))
+    colsample_bytree: TrialParameter = Field(
+        TrialParameter(name="colsample_bytree", value_type="float", start=0.7, end=0.9, step=0.1))
+    reg_lambda: TrialParameter = Field(TrialParameter(name="reg_lambda", value_type="int", start=1, end=3, step=1))
+    reg_alpha: TrialParameter = Field(TrialParameter(name="reg_alpha", value_type="float", start=0, end=1, step=0.1))
+    n_estimators: TrialParameter = Field(
+        TrialParameter(name="n_estimators", value_type="int", start=50, end=150, step=50))
+    max_leaf_nodes: TrialParameter = Field(
+        TrialParameter(name="max_leaf_nodes", value_type="int", start=3, end=7))
+    scale_pos_weight: TrialParameter = Field(TrialParameter(name="scale_pos_weight", value_type="float", values=[1.0]))
     random_state: int = Field(42, ge=0)
 
     class Config:
